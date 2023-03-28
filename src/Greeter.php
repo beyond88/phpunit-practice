e8106d5c3f954a767eb9e85aaed3325bd5680b50
beyond88 <muhin.cse.diu@gmail.com>
2023-03-28 16:29:50 +0600

    Update composer

---
+++ src/Greeter.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Root\PhpunitPractice;
+
+class Greeter 
+{
+
+    private $name;
+    
+    public function __construct()
+    {
+
+    }
+
+    public function GreetsWithName(): void 
+    {
+
+    }
+
+    public function greet( $name )
+    {
+        return $this->name  = $name; 
+    }
+}
--- tests/GreeterTest.php
+++ tests/GreeterTest.php
@@ -1,14 +1,13 @@
 <?php declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
+use Root\PhpunitPractice\Greeter;
 
 final class GreeterTest extends TestCase
 {
     public function testGreetsWithName(): void
     {
         $greeter = new Greeter;
-
-        $greeting = $greeter->greet('Alice');
-
+        $greeting = $greeter->greet('Hello, Alice!');
         $this->assertSame('Hello, Alice!', $greeting);
     }
 }
