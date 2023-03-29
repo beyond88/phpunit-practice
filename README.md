79b44b5f609b215b5a43df0d386b39d39b60565f
beyond88 <muhin.cse.diu@gmail.com>
2023-03-29 16:29:51 +0600

    Added more test

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # PHPUnit testing practice
 
 ### 22-03-2023
+
 Asserting Return Values, Expecting Exceptions
+
+### 29-03-2023
+
+Testing Output, Incomplete Tests, Skipping Tests
--- tests/NumericDataSetsTest.php
+++ tests/NumericDataSetsTest.php
@@ -7,10 +7,10 @@ final class NumericDataSetsTest extends TestCase
     public static function additionProvider(): array
     {
         return [
-            [0, 0, 0],
-            [0, 1, 1],
-            [1, 0, 1],
-            [1, 1, 3],
+            'adding zeros'  => [0, 0, 0],
+            'zero plus one' => [0, 1, 1],
+            'one plus zero' => [1, 0, 1],
+            'one plus one'  => [1, 1, 3],
         ];
     }
 
